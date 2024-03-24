c5212cb825d6ffdf7b1cdf511e33f54c261dfd06
Thiago <111020021+thiagoconsult@users.noreply.github.com>
2024-03-24 17:31:03 +0000

    Alteração da página index e criação do arquivo `_app.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import styles from "../styles/index.module.css";
+import Image from "next/image";
+import Logo from "../assets/images/collabnews-logo.png";
+
 function Home() {
-  return <h1>Em Construção!</h1>;
+  return (
+    <>
+      <div className={styles.container}>
+        <header className={styles.topo}>
+          <Image src={Logo}></Image>
+        </header>
+
+        <main className={styles.corpo}>Em construção....</main>
+
+        <footer className={styles.rodape}>collabnews - 2024 </footer>
+      </div>
+    </>
+  );
 }
 
 export default Home;
